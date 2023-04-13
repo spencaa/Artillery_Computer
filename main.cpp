b006a366c3ea246538eefa32b2ffdd0410887e79
spencaa <campspencer2@gmail.com>
2023-04-13 17:22:19 -0500

    Added functionality to the M119A choice. Cleaned up the menu function.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
-// M777 FUNCTION IS INOP; PROGRAM WILL NOT WORK IF CHOSEN
 #include <string>
-#include "Headers/m252functions.h"
+#include "Classes and Headers/artilleryFunctions.h"
 
 using namespace std;
 
@@ -8,38 +7,45 @@ int main() {
     string line_;
     int menu_choice = 0;
     double distance, range, elevation, timeOfFire;
+    string range_input;
 
-    cout << "Select your weapons system:\n1. Mk6/Podnos Mortar\n2. M777 Howitzer [INOP]\n3. Quit\n\n";
-    do {
-        cin >> menu_choice;
-    } while (!(menu_choice == 1 || menu_choice == 3));
+    // Opens the artillery weapon selection menu
+    openArtilleryMenu(menu_choice);
 
+    // Closes the program upon selecting option 3
     if (menu_choice == 3)
         return 0;
 
     //  Need to add weather before this for better calculations
-    //  Podnos/M252
     while (menu_choice != 3) {
+        requestDistance(distance, range_input, menu_choice);
+    //  M252/Podnos Calculations
         if (menu_choice == 1) {
-            cout << "\nEnter your desired distance in increments of 50 meters (100-4050): ";
-            cin >> distance;
     //      Podnos charge 0
-            if (distance <= 500) {
+            if (distance <= 500)
                 charge0Open(distance, range, elevation, timeOfFire, line_);
-            }
     //      Podnos charge 1
-            else if (distance > 500 && distance <= 2000) {
+            else if (distance > 500 && distance <= 2000)
                 charge1Open(distance, range, elevation, timeOfFire, line_);
-            }
     //      Podnos charge 2
-            else if (distance > 2000 && distance <= 4050) {
+            else if (distance > 2000 && distance <= 4050)
                 charge2Open(distance, range, elevation, timeOfFire, line_);
-            }
-    //      System pause and clear formatting
-            cout << endl;
-            system("pause");
-            system("cls");
         }
+        if (menu_choice == 2) {
+    //      M119A Charge 0
+            if (distance <= 2350)
+                m119charge0Open(distance, range, elevation, timeOfFire, line_);
+    //      M119A Charge 1
+            else if (distance > 2350 && distance <= 6000)
+                m119charge1Open(distance, range, elevation, timeOfFire, line_);
+    //      M119A Charge 2
+            else if (distance > 6000 && distance <= 15400)
+                m119charge2Open(distance, range, elevation, timeOfFire, line_);
+        }
+    //      System pause and clear formatting
+        cout << endl;
+        system("pause");
+        system("cls");
     }
     return 0;
 }
